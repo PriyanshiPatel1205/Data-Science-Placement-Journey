67d97fc46dda73a981df13254e8ff1606348fccf
Priyanshi Patel <priyanshipatel1252003@gmail.com>
2026-04-13 22:45:35 +0530

    Day 2 - Python Strings and Conditional Statements

--- Week-1/Day2_String_loops.py
+++ Week-1/Day2_String_loops.py
@@ -37,6 +37,50 @@ print("Lowercase: ", str1.lower()) # using lower() to convert the string to lowe
 print("Title Case: ", str1.title()) # using title() to convert the string to title case (first letter of each word is capitalized)
 print("Ends with 'World': ", str1.endswith("World")) # using endswith() to check if the string ends with a specific substring
 print("Starts with 'Hello': ", str1.startswith("Hello")) # using startswith() to check if the string starts with a specific substring
+print("Replace 'World' with 'Python': ", str1.replace("World", "Python")) # using replace() to replace a specific substring with another substring
+print("Find 'o': ", str1.find("o")) # using find() to get the index of the first occurrence of a specific substring (returns -1 if not found)
+print("Count 'o': ", str1.count("o")) # using count() to get the number of occurrences of a specific substring in the string    
 
+#Write a program to input user's firstname and print its length
+#firstname = input("Enter your firstname: ")
+#print("Length of your firstname: ", len(firstname))
 
+#WAP to count occurrences of $
+dollarstr = "$$hello$$world$$"
+print("Count of $: ",dollarstr.count("$"))
 
+#=================================================================#
+#CONDITIONAL STATEMENTS
+
+#if-elif-else statements
+
+age = 21
+if(age>=18):
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+#WAP to check if a number enterred by user is odd or even
+num=int(input("Enter a number: "))
+if(num%2==0):
+    print("It is an even number.")
+else:
+    print("It is an odd number.")
+
+#WAP to find the greatest of 3 numbers entered by the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+if(num1>=num2 and num1>=num3):
+    print("Largest number: ", num1)
+elif(num2>=num1 and num2>=num3):
+    print("Largest number: ", num2)
+else:
+    print("Largest number: ", num3)
+
+#WAP to check if a number is a multiple of 7 or not
+num4= int(input("Enter a number: "))
+if(num4%7==0):
+    print("It is multiple of 7.")
+else:
+    print("It is not a multiple of 7.")
